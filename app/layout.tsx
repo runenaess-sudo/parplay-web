17f046c7fd651de36bb418d7bbbd2f4cd9525351
Rune <din@email.no>
2026-06-22 15:23:42 +0200

    Oppdatert landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
+import { Header } from "@/src/components/Header/Header";
+import "mapbox-gl/dist/mapbox-gl.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Header } from "@/src/components/Header/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
