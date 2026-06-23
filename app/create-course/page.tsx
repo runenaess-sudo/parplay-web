f74a17050f37a5a555f644abc138ff0c4bd15aeb
Rune <din@email.no>
2026-06-23 15:50:46 +0200

    Oppdatert landing page

---
+++ app/create-course/page.tsx
@@ -0,0 +1,7 @@
+export default function CreateCoursePage() {
+    return (
+        <div className="p-6">
+            <h1 className="text-xl font-semibold">Create Course</h1>
+        </div>
+    );
+}
