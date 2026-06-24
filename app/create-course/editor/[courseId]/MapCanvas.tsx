90bd66c3adb337ede6cad146f665699917e88b2b
Rune <din@email.no>
2026-06-24 14:15:53 +0200

    Oppdatert landing page

--- app/create-course/editor/[courseId]/MapCanvas.tsx
+++ app/create-course/editor/[courseId]/MapCanvas.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+import { createClient } from "@supabase/supabase-js";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useEffect, useRef } from "react";
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN!;
+
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
 
 export function MapCanvas() {
     const ref = useRef<HTMLDivElement | null>(null);
@@ -19,7 +25,23 @@ export function MapCanvas() {
             zoom: 14,
         });
 
-        console.log("EDITOR MAP LOADED");
+        map.on("load", async () => {
+            console.log("EDITOR MAP LOADED");
+
+            const { data: course, error } = await supabase
+                .from("courses")
+                .select("*, holes(*)")
+                .eq("id", "5ad7fc02-778b-4bf8-a93b-3cb2514cc081")
+                .single();
+
+            if (error) {
+                console.error("Feil ved henting av bane:", error);
+                return;
+            }
+
+            console.log("EDITOR BANEDATA:", course);
+            console.log("EDITOR HOLES:", course.holes);
+        });
 
         return () => map.remove();
     }, []);
