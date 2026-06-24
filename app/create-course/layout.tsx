0a21337e4b10a2d23ccfcda9c09524ae2945d9c8
Rune <din@email.no>
2026-06-24 13:27:01 +0200

    Oppdatert landing page

--- app/create-course/layout.tsx
+++ app/create-course/layout.tsx
@@ -1,11 +1,7 @@
 "use client";
-export default function CreateCourseLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+export default function CreateCourseLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="h-screen flex flex-col overflow-hidden bg-slate-950 text-slate-100 relative">
+        <div className="h-screen flex flex-col bg-slate-950 text-slate-100 relative min-h-0">
             {children}
         </div>
     );
