e6a2dee5ac7449a55c75b2962602352d59a1ff70
Rune <din@email.no>
2026-06-25 20:23:11 +0200

    Oppdatert landing page

--- src/lib/supabase-browser.ts
+++ src/lib/supabase-browser.ts
@@ -4,7 +4,14 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabaseBrowser = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+        auth: {
+            persistSession: true,
+            autoRefreshToken: true,
+            detectSessionInUrl: true,
+        },
+    }
 );
 
 console.log("SUPABASE BROWSER URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
