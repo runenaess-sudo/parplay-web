c063ccc6fe673acc54b50c5d60f75598fccb79d6
Rune <din@email.no>
2026-09-11 13:36:32 +0200

    Oppdatere Web og admin med claims

--- src/lib/supabase-browser.ts
+++ src/lib/supabase-browser.ts
@@ -1,15 +1,8 @@
 "use client";
 
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
-export const supabaseBrowser = createClient(
+export const supabaseBrowser = createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-        auth: {
-            persistSession: true,
-            autoRefreshToken: true,
-            detectSessionInUrl: true,
-        },
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
